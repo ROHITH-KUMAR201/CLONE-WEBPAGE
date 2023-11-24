79ac57765aa6167f8b161ccfda43c5739a5580a4
ROHITH-KUMAR201 <151857839+ROHITH-KUMAR201@users.noreply.github.com>
2023-11-24 23:01:58 +0530

    Add files via upload

---
+++ CSS & HTML/alert.js
@@ -0,0 +1,5 @@
+function getValue( )
+{
+    var retVal = prompt("Enter your name your mail address to process.");
+    alert("Rest password link will be sent to your mail shortly.");
+ }
---
+++ CSS & HTML/validate.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Validation Page</title>
+    </head>
+    <body>
+    <?php
+     echo $_POST["email"];
+     echo $_POST["password"];
+     ?>
+     <?php
+      echo $_POST["name"];
+     echo $_POST["email"];
+     echo $_POST["password"];
+     ?>
+    </body>
+</html>
